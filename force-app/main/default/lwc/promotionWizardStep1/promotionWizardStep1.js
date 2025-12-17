8b2fc9f1e0656cfdc803693b2365939be99b6a7b
Sanjeet Mishra <sanjeetmishra1999@gmail.com>
2025-12-17 22:25:39 +0530

    Task 1.3

--- force-app/main/default/lwc/promotionWizardStep1/promotionWizardStep1.js
+++ force-app/main/default/lwc/promotionWizardStep1/promotionWizardStep1.js
@@ -1,4 +1,6 @@
 import { LightningElement, api } from 'lwc';
+import promotionStateManager from 'c/promotionStateManager';
+import { atom, setAtom, computed, defineState } from 'c/sfState';
 
 /** TODO FOR THE CHALLENGE: import the state manager, and the context modules */
 
@@ -23,6 +25,9 @@ export default class PromotionWizardStep1 extends LightningElement {
         }
         
         // TODO FOR THE CHALLENGE: Update the promotion name in the state
+        if (this.promotionState && this.promotionState.value && typeof this.promotionState.value.updatePromotionName === 'function') {
+            this.promotionState.value.updatePromotionName(this.promotionName);
+        }
         
         return true;
     }
